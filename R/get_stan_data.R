0b7473ff87bcdff9987df34b13f348bf8e61413e
Xinxin Chen <xinxinchen476@gmail.com>
2024-05-08 17:06:24 +0800

    added function to get Stan data for BHM

--- R/get_stan_data.R
+++ R/get_stan_data.R
@@ -1,5 +1,7 @@
 #' get Stan data for PP
 #'
+#' @include data_checks.R
+#'
 #' @noRd
 get.stan.data.pp = function(
     formula,
@@ -77,6 +79,104 @@ get.stan.data.pp = function(
     'mean_beta'       = beta.mean,
     'sd_beta'         = beta.sd,
     'a0_vals'         = a0.vals,
+    'disp_mean'       = disp.mean,
+    'disp_sd'         = disp.sd,
+    'dist'            = dist,
+    'link'            = link,
+    'offs'            = offset
+  )
+  return(standat)
+}
+
+
+#' get Stan data for BHM
+#'
+#' @include data_checks.R
+#'
+#' @noRd
+get.stan.data.bhm = function(
+    formula,
+    family,
+    data.list,
+    offset.list       = NULL,
+    meta.mean.mean    = NULL,
+    meta.mean.sd      = NULL,
+    meta.sd.mean      = NULL,
+    meta.sd.sd        = NULL,
+    disp.mean         = NULL,
+    disp.sd           = NULL
+) {
+  data.checks(formula, family, data.list, offset.list)
+
+  res          = stack.data(formula = formula,
+                            data.list = data.list)
+  y            = res$y
+  X            = res$X
+  start.index  = res$start.index
+  end.index    = res$end.index
+  p            = ncol(X)
+  N            = length(y)
+  K            = length(end.index)
+  fam.indx     = get.dist.link(family)
+  dist         = fam.indx[1]
+  link         = fam.indx[2]
+
+  ## Default offset for each data set is a vector of 0s
+  if ( is.null(offset.list) ){
+    offset = rep(0, N)
+  }else {
+    offset = unlist(offset.list)
+  }
+
+  ## Default normal hyperprior on mean of regression coefficients is N(0, 10^2)
+  if ( !is.null(meta.mean.mean) ){
+    if ( !( is.vector(meta.mean.mean) & (length(meta.mean.mean) %in% c(1, p)) ) )
+      stop("meta.mean.mean must be a scalar or a vector of length ", p, " if meta.mean.mean is not NULL")
+  }
+  meta.mean.mean = to.vector(param = meta.mean.mean, default.value = 0, len = p)
+  if ( !is.null(meta.mean.sd) ){
+    if ( !( is.vector(meta.mean.sd) & (length(meta.mean.sd) %in% c(1, p)) ) )
+      stop("meta.mean.sd must be a scalar or a vector of length ", p, " if meta.mean.sd is not NULL")
+  }
+  meta.mean.sd = to.vector(param = meta.mean.sd, default.value = 10, len = p)
+
+  ## Default half-normal hyperprior on sd of regression coefficients is N^{+}(0, 1)
+  if ( !is.null(meta.sd.mean) ){
+    if ( !( is.vector(meta.sd.mean) & (length(meta.sd.mean) %in% c(1, p)) ) )
+      stop("meta.sd.mean must be a scalar or a vector of length ", p, " if meta.sd.mean is not NULL")
+  }
+  meta.sd.mean = to.vector(param = meta.sd.mean, default.value = 0, len = p)
+  if ( !is.null(meta.sd.sd) ){
+    if ( !( is.vector(meta.sd.sd) & (length(meta.sd.sd) %in% c(1, p)) ) )
+      stop("meta.sd.sd must be a scalar or a vector of length ", p, " if meta.sd.sd is not NULL")
+  }
+  meta.sd.sd = to.vector(param = meta.sd.sd, default.value = 1, len = p)
+
+  ## Default half-normal hyperprior on dispersion parameters (if exist) is N^{+}(0, 10^2)
+  if ( !is.null(disp.mean) ){
+    if ( !( is.vector(disp.mean) & (length(disp.mean) %in% c(1, K)) ) )
+      stop("disp.mean must be a scalar or a vector of length ", K, " if disp.mean is not NULL")
+  }
+  disp.mean = to.vector(param = disp.mean, default.value = 0, len = K)
+  if ( !is.null(disp.sd) ){
+    if ( !( is.vector(disp.sd) & (length(disp.sd) %in% c(1, K)) ) )
+      stop("disp.sd must be a scalar or a vector of length ", K, " if disp.sd is not NULL")
+  }
+  disp.sd = to.vector(param = disp.sd, default.value = 10, len = K)
+
+
+  standat = list(
+    'K'               = K,
+    'N'               = N,
+    'start_idx'       = start.index,
+    'end_idx'         = end.index,
+    'p'               = p,
+    'y'               = y,
+    'X'               = X,
+    'meta_mean_mean'  = meta.mean.mean,
+    'meta_mean_sd'    = meta.mean.sd,
+    'meta_sd_mean'    = meta.sd.mean,
+    'meta_sd_sd'      = meta.sd.sd,
     'disp_mean'       = disp.mean,
     'disp_sd'         = disp.sd,
     'dist'            = dist,
