9455ccd73773fb11672c5cd38a1ebf8f19661c6f
Xinxin Chen <xinxinchen476@gmail.com>
2024-06-26 13:46:31 -0400

    updated glm.logml.map() to only require post.samples as the input

--- R/glm_logml_map.R
+++ R/glm_logml_map.R
@@ -1,31 +1,30 @@
-#' Log marginal likelihood of a GLM under meta-analytic predictive (MAP) prior (the prior induced by
-#' Bayesian hierarchical model (BHM))
+#' Log marginal likelihood of a GLM under meta-analytic predictive (MAP) prior
 #'
 #' Uses Markov chain Monte Carlo (MCMC) and bridge sampling to estimate the logarithm of the marginal
-#' likelihood of a GLM under MAP.
+#' likelihood of a GLM under the meta-analytic predictive (MAP) prior. The MAP prior is the prior
+#' induced by the Bayesian hierarchical model (BHM).
 #'
-#' This function shares the same arguments as [glm.bhm()], while introducing two additional parameters:
-#' `post.samples` and `bridge.args`. `post.samples` provides posterior samples of the GLM under BHM
-#' (e.g., the output from [glm.bhm()]), whereas `bridge.args` specifies arguments to pass onto
-#' [bridgesampling::bridge_sampler()] (other than `samples`, `log_posterior`, `data`, `lb`, and `ub`).
+#' The arguments related to MCMC sampling are utilized to draw samples from the prior induced by the
+#' Bayesian hierarchical model (BHM) (equivalently, the meta-analytic predictive (MAP) prior). These
+#' samples are then utilized to compute the normalizing constant of the BHM using historical data
+#' sets only.
 #'
-#' It is important to ensure that the values assigned to the shared arguments (excluding those relevant
-#' for MCMC sampling) in this function and [glm.bhm()] align with those used in generating `post.samples`.
-#' The arguments pertinent to MCMC sampling are utilized to compute the log marginal likelihood under
-#' BHM using only historical data sets.
-#'
-#' @include glm_logml_bhm.R
+#' @include glm_bhm_lognc.R
 #'
 #' @export
 #'
-#' @inheritParams glm.bhm
-#' @param post.samples      an object of class `draws_df`, `draws_matrix`, `matrix`, or `data.frame` giving posterior
-#'                          samples of a GLM under BHM, such as the output from [glm.bhm()]. Each row corresponds to the
-#'                          posterior samples obtained from one iteration of MCMC. The column names of `post.samples` should
-#'                          include the names of covariates for regression coefficients, such as "(Intercept)", and
-#'                          "dispersion" for the dispersion parameter, if applicable.
-#' @param bridge.args       a `list` giving arguments (other than samples, log_posterior, data, lb, ub) to pass
-#'                          onto [bridgesampling::bridge_sampler()].
+#' @param post.samples      output from [glm.bhm()] giving posterior samples of a GLM under the Bayesian hierarchical
+#'                          model (BHM) or samples from the prior induced by the BHM, with an attribute called 'data'
+#'                          which includes the list of variables specified in the data block of the Stan program.
+#' @param bridge.args       a `list` giving arguments (other than `samples`, `log_posterior`, `data`, `lb`, and `ub`) to
+#'                          pass onto [bridgesampling::bridge_sampler()].
+#' @param iter_warmup       number of warmup iterations to run per chain. Defaults to 1000. See the argument `iter_warmup`
+#'                          in `sample()` method in cmdstanr package.
+#' @param iter_sampling     number of post-warmup iterations to run per chain. Defaults to 1000. See the argument `iter_sampling`
+#'                          in `sample()` method in cmdstanr package.
+#' @param chains            number of Markov chains to run. Defaults to 4. See the argument `chains` in `sample()` method
+#'                          in cmdstanr package.
+#' @param ...               arguments passed to `sample()` method in cmdstanr package (e.g., `seed`, `refresh`, `init`).
 #'
 #' @return
 #'  The function returns a `list` with the following objects
@@ -33,13 +32,14 @@
 #'  \describe{
 #'    \item{model}{"MAP"}
 #'
-#'    \item{logml}{the estimated logarithm of the marginal likelihood}
+#'    \item{logml}{the estimated logarithm of the marginal likelihood of the meta-analytic predictive (MAP) prior}
 #'
-#'    \item{bs}{an object of class `bridge` or `bridge_list` giving the output from
-#'    [bridgesampling::bridge_sampler()] using all data sets (including current and historical data)}
+#'    \item{bs}{an object of class `bridge` or `bridge_list` containing the output from using [bridgesampling::bridge_sampler()]
+#'    to compute the logarithm of the normalizing constant of the Bayesian hierarchical model (BHM) using all data sets}
 #'
-#'    \item{bs.hist}{an object of class `bridge` or `bridge_list` giving the output from
-#'    [bridgesampling::bridge_sampler()] using historical data sets}
+#'    \item{bs.hist}{an object of class `bridge` or `bridge_list` containing the output from using
+#'    [bridgesampling::bridge_sampler()] to compute the logarithm of the normalizing constant of the BHM using historical
+#'    data sets only}
 #'  }
 #'
 #' @references
@@ -63,80 +63,75 @@
 #'   )
 #'   glm.logml.map(
 #'     post.samples = d.bhm,
-#'     formula = formula, family = family,
-#'     data.list = data_list,
 #'     bridge.args = list(silent = TRUE),
 #'     chains = 1, iter_warmup = 1000, iter_sampling = 2000
 #'   )
 #' }
 glm.logml.map = function(
     post.samples,
-    formula,
-    family,
-    data.list,
-    offset.list       = NULL,
-    meta.mean.mean    = NULL,
-    meta.mean.sd      = NULL,
-    meta.sd.mean      = NULL,
-    meta.sd.sd        = NULL,
-    disp.mean         = NULL,
-    disp.sd           = NULL,
     bridge.args       = NULL,
     iter_warmup       = 1000,
     iter_sampling     = 1000,
     chains            = 4,
     ...
 ) {
-  K = length(data.list)
+  stan.data = attr(post.samples, 'data')
+  K         = stan.data$K
   if ( K == 1 ){
     stop("data.list should include at least one historical data set")
   }
-  ## computing normalizing constant using all data sets
-  res.all = glm.logml.bhm(
-    formula        = formula,
-    family         = family,
-    data.list      = data.list,
+  ## computing normalizing constant for BHM using all data sets
+  res.all = glm.bhm.lognc(
     post.samples   = post.samples,
-    offset.list    = offset.list,
-    meta.mean.mean = meta.mean.mean,
-    meta.mean.sd   = meta.mean.sd,
-    meta.sd.mean   = meta.sd.mean,
-    meta.sd.sd     = meta.sd.sd,
-    disp.mean      = disp.mean,
-    disp.sd        = disp.sd,
     bridge.args    = bridge.args
   )
 
-  ## computing normalizing constant using historical data sets only
-  if ( !is.null(disp.mean) && (length(disp.mean) != 1) ){
-    disp.mean = disp.mean[-1]
+  ## get Stan data for BHM using historical data sets only
+  hist.stan.data           = stan.data
+  hist.stan.data$K         = K - 1
+  n                        = stan.data$end_idx[1] ## current data sample size
+  hist.stan.data$N         = stan.data$N - n
+  hist.stan.data$start_idx = stan.data$start_idx[-1] - n
+  hist.stan.data$end_idx   = stan.data$end_idx[-1] - n
+  hist.stan.data$y         = stan.data$y[-(1:n)]
+  hist.stan.data$X         = stan.data$X[-(1:n), ]
+  hist.stan.data$disp_mean = stan.data$disp_mean[-1]
+  hist.stan.data$disp_sd   = stan.data$disp_sd[-1]
+  hist.stan.data$offs      = stan.data$offs[-(1:n)]
+
+  ## fit BHM using historical data sets only
+  glm_bhm = instantiate::stan_package_model(
+    name = "glm_bhm",
+    package = "hdbayes"
+  )
+  fit = glm_bhm$sample(data = hist.stan.data,
+                       iter_warmup = iter_warmup, iter_sampling = iter_sampling, chains = chains,
+                       ...)
+  if ( hist.stan.data$dist > 2 ) {
+    ## rename parameters
+    K        = hist.stan.data$K
+    oldnames = paste0( 'dispersion[', 1:K, ']' )
+    if (K == 1) {
+      newnames = 'dispersion'
+    }else {
+      newnames = c('dispersion', paste0( 'dispersion', '_hist_', 1:(K-1) ))
+    }
+    hist.post.samples = rename.params(fit = fit, oldnames = oldnames, newnames = newnames)
+  }else {
+    hist.post.samples = fit$draws(format = 'draws_df')
   }
-  if ( !is.null(disp.sd) && (length(disp.sd) != 1) ){
-    disp.sd = disp.sd[-1]
-  }
-  res.hist = glm.logml.bhm(
-    formula        = formula,
-    family         = family,
-    data.list      = data.list[-1],
-    post.samples   = NULL,
-    offset.list    = offset.list[-1],
-    meta.mean.mean = meta.mean.mean,
-    meta.mean.sd   = meta.mean.sd,
-    meta.sd.mean   = meta.sd.mean,
-    meta.sd.sd     = meta.sd.sd,
-    disp.mean      = disp.mean,
-    disp.sd        = disp.sd,
-    bridge.args    = bridge.args,
-    iter_warmup    = iter_warmup,
-    iter_sampling  = iter_sampling,
-    chains         = chains,
-    ...
+  attr(x = hist.post.samples, which = 'data') = hist.stan.data
+
+  ## computing normalizing constant for BHM using historical data sets only
+  res.hist = glm.bhm.lognc(
+    post.samples   = hist.post.samples,
+    bridge.args    = bridge.args
   )
 
-  ## Return a list of model name, estimated log marginal likelihood, and output from bridgesampling::bridge_sampler
+  ## Return a list of model name, estimated log marginal likelihood, and outputs from bridgesampling::bridge_sampler
   res = list(
     'model'   = "MAP",
-    'logml'   = res.all$logml - res.hist$logml,
+    'logml'   = res.all$lognc - res.hist$lognc,
     'bs'      = res.all$bs,
     'bs.hist' = res.hist$bs
   )
