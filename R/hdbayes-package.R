8d8a521ef1c4f53c824cc07677b5adc8d193e60d
Xinxin Chen <xinxinchen476@gmail.com>
2024-07-09 09:41:29 -0400

    updated DESCRIPTION

--- R/hdbayes-package.R
+++ R/hdbayes-package.R
@@ -12,7 +12,7 @@
 #' @importFrom callr r
 #' @importFrom fs dir_copy
 #' @importFrom formula.tools is.two.sided lhs.vars
-#' @importFrom stats model.matrix family glm binomial dgamma dnorm pnorm lm gaussian dbeta pbeta
+#' @importFrom stats model.matrix family glm binomial dgamma dnorm pnorm lm gaussian dbeta pbeta rbinom
 #' @importFrom posterior variables
 #' @importFrom enrichwith enrich
 #' @importFrom mclust Mclust mclustBIC
