7f41b85db9cd65561092f12a72b289fb5bcbc2c5
Xinxin Chen <xinxinchen476@gmail.com>
2025-02-25 18:01:49 -0500

    added function for fitting PWE model under CP

---
+++ R/pwe_commensurate.R
@@ -0,0 +1,162 @@
+#' Posterior of commensurate prior (CP)
+#'
+#' Sample from the posterior distribution of a piecewise exponential (PWE) model (i.e., a proportional hazards model
+#' with a piecewise constant baseline hazard) using the commensurate prior (CP) by Hobbs et al. (2011)
+#' <doi:10.1111/j.1541-0420.2011.01564.x>.
+#'
+#' The commensurate prior (CP) assumes that the regression coefficients for the current data model conditional on those
+#' for the historical data model are independent normal distributions with mean equal to the corresponding regression
+#' coefficients for the historical data and variance equal to the inverse of the corresponding elements of a vector of
+#' precision parameters (referred to as the commensurability parameter \eqn{\tau}). We regard \eqn{\tau} as random and elicit
+#' a spike-and-slab prior, which is specified as a mixture of two half-normal priors, on \eqn{\tau}.
+#'
+#' The number of current data regression coefficients is assumed to be the same as that of historical data regression
+#' coefficients. The baseline hazard parameters for both current and historical data models are assumed to be independent and
+#' identically distributed, each assigned a half-normal prior.
+#'
+#' @include data_checks_pwe.R
+#' @include get_stan_data_pwe.R
+#'
+#' @export
+#'
+#' @param formula           a two-sided formula giving the relationship between the response variable and covariates.
+#'                          The response is a survival object as returned by the `survival::Surv(time, event)` function,
+#'                          where event is a binary indicator for event (0 = no event, 1 = event has occurred). The type of
+#'                          censoring is assumed to be right-censoring.
+#' @param data.list         a list of `data.frame`s. The first element in the list is the current data, and the rest
+#'                          are the historical data sets. For fitting accelerated failure time (AFT) models, all historical
+#'                          data sets will be stacked into one historical data set.
+#' @param breaks            a numeric vector specifying the time points that define the boundaries of the piecewise
+#'                          intervals. The values should be in ascending order, with the final value being greater than
+#'                          or equal to the maximum observed time.
+#' @param beta0.mean        a scalar or a vector whose dimension is equal to the number of regression coefficients
+#'                          giving the mean parameters for the prior on the historical data regression coefficients. If a
+#'                          scalar is provided, `beta0.mean` will be a vector of repeated elements of the given scalar.
+#'                          Defaults to a vector of 0s.
+#' @param beta0.sd          a scalar or a vector whose dimension is equal to the number of regression coefficients giving
+#'                          the sd parameters for the prior on the historical data regression coefficients. If a scalar is
+#'                          provided, same as for `beta0.mean`. Defaults to a vector of 10s.
+#' @param p.spike           a scalar between 0 and 1 giving the probability of the spike component in spike-and-slab prior
+#'                          on commensurability parameter \eqn{\tau}. Defaults to 0.1.
+#' @param spike.mean        a scalar giving the location parameter for the half-normal prior (spike component) on \eqn{\tau}.
+#'                          Defaults to 200.
+#' @param spike.sd          a scalar giving the scale parameter for the half-normal prior (spike component) on \eqn{\tau}.
+#'                          Defaults to 0.1.
+#' @param slab.mean         a scalar giving the location parameter for the half-normal prior (slab component) on \eqn{\tau}.
+#'                          Defaults to 0.
+#' @param slab.sd           a scalar giving the scale parameter for the half-normal prior (slab component) on \eqn{\tau}.
+#'                          Defaults to 5.
+#' @param base.hazard.mean  a scalar or a vector whose dimension is equal to the number of intervals giving the location
+#'                          parameters for the half-normal priors on the baseline hazards of the PWE model. If a scalar is
+#'                          provided, same as for `beta0.mean`. Defaults to 0.
+#' @param base.hazard.sd    a scalar or a vector whose dimension is equal to the number of intervals giving the scale
+#'                          parameters for the half-normal priors on the baseline hazards of the PWE model. If a scalar is
+#'                          provided, same as for `beta0.mean`. Defaults to 10.
+#' @param get.loglik        whether to generate log-likelihood matrix. Defaults to FALSE.
+#' @param iter_warmup       number of warmup iterations to run per chain. Defaults to 1000. See the argument `iter_warmup` in
+#'                          `sample()` method in cmdstanr package.
+#' @param iter_sampling     number of post-warmup iterations to run per chain. Defaults to 1000. See the argument `iter_sampling`
+#'                          in `sample()` method in cmdstanr package.
+#' @param chains            number of Markov chains to run. Defaults to 4. See the argument `chains` in `sample()` method in
+#'                          cmdstanr package.
+#' @param ...               arguments passed to `sample()` method in cmdstanr package (e.g., `seed`, `refresh`, `init`).
+#'
+#' @return
+#'  The function returns an object of class `draws_df` giving posterior samples, with an attribute called 'data' which includes
+#'  the list of variables specified in the data block of the Stan program.
+#'
+#' @references
+#'  Hobbs, B. P., Carlin, B. P., Mandrekar, S. J., and Sargent, D. J. (2011). Hierarchical commensurate and power prior models for adaptive incorporation of historical information in clinical trials. Biometrics, 67(3), 1047–1056.
+#'
+#' @examples
+#' if (instantiate::stan_cmdstan_exists()) {
+#'   if(requireNamespace("survival")){
+#'     library(survival)
+#'     data(E1684)
+#'     data(E1690)
+#'     ## take subset for speed purposes
+#'     E1684 = E1684[1:100, ]
+#'     E1690 = E1690[1:50, ]
+#'     ## replace 0 failure times with 0.50 days
+#'     E1684$failtime[E1684$failtime == 0] = 0.50/365.25
+#'     E1690$failtime[E1690$failtime == 0] = 0.50/365.25
+#'     E1684$cage = as.numeric(scale(E1684$age))
+#'     E1690$cage = as.numeric(scale(E1690$age))
+#'     data_list = list(currdata = E1690, histdata = E1684)
+#'     nbreaks = 3
+#'     probs   = 1:nbreaks / nbreaks
+#'     breaks  = as.numeric(
+#'       quantile(E1690[E1690$failcens==1, ]$failtime, probs = probs)
+#'     )
+#'     breaks  = c(0, breaks)
+#'     breaks[length(breaks)] = max(10000, 1000 * breaks[length(breaks)])
+#'     pwe.commensurate(
+#'       formula = survival::Surv(failtime, failcens) ~ treatment + sex + cage + node_bin,
+#'       data.list = data_list,
+#'       breaks = breaks,
+#'       p.spike = 0.1,
+#'       chains = 1, iter_warmup = 500, iter_sampling = 1000
+#'     )
+#'   }
+#' }
+pwe.commensurate = function(
+    formula,
+    data.list,
+    breaks,
+    beta0.mean        = NULL,
+    beta0.sd          = NULL,
+    p.spike           = 0.1,
+    spike.mean        = 200,
+    spike.sd          = 0.1,
+    slab.mean         = 0,
+    slab.sd           = 5,
+    base.hazard.mean  = NULL,
+    base.hazard.sd    = NULL,
+    get.loglik        = FALSE,
+    iter_warmup       = 1000,
+    iter_sampling     = 1000,
+    chains            = 4,
+    ...
+) {
+  ## get Stan data for CP
+  standat = get.pwe.stan.data.cp(
+    formula          = formula,
+    data.list        = data.list,
+    breaks           = breaks,
+    beta0.mean       = beta0.mean,
+    beta0.sd         = beta0.sd,
+    p.spike          = p.spike,
+    spike.mean       = spike.mean,
+    spike.sd         = spike.sd,
+    slab.mean        = slab.mean,
+    slab.sd          = slab.sd,
+    base.hazard.mean = base.hazard.mean,
+    base.hazard.sd   = base.hazard.sd,
+    get.loglik       = get.loglik
+  )
+
+  pwe_commensurate = instantiate::stan_package_model(
+    name = "pwe_commensurate",
+    package = "hdbayes"
+  )
+
+  ## fit model in cmdstanr
+  fit = pwe_commensurate$sample(data = standat,
+                                iter_warmup = iter_warmup, iter_sampling = iter_sampling, chains = chains,
+                                ...)
+
+  ## rename parameters
+  p        = standat$p
+  X1       = standat$X1
+  J        = standat$J
+  oldnames = c(paste0("beta[", 1:p, "]"), paste0("beta0[", 1:p, "]"),
+               paste0("lambda[", 1:J, "]"), paste0("lambda0[", 1:J, "]"))
+  newnames = c(colnames(X1), paste0(colnames(X1), '_hist'),
+               paste0("basehaz[", 1:J, "]"), paste0("basehaz_hist[", 1:J, "]"))
+
+  ## reorder parameters so that regression coefficients appear at the top
+  d = rename.params(fit = fit, oldnames = oldnames, newnames = newnames)
+  ## add data used for the variables specified in the data block of the Stan program as an attribute
+  attr(x = d, which = 'data') = standat
+  return(d)
+}
---
+++ src/stan/pwe_commensurate.stan
@@ -0,0 +1,123 @@
+// fit PWE model
+functions {
+  //' PWE log likelihood
+  //' @param y               failure / censoring time
+  //' @param eta             linear predictor
+  //' @param lambda          vector of baseline hazards
+  //' @param log_lambda      vector of log(lambda)
+  //' @param breaks          (J+1)-dim vector giving intervals
+  //' @param j               index of the interval for which the individual failed / was censored: j \in {1, ..., J}
+  //' @param death_ind       integer giving whether individual died (death_ind == 1) or was censored (death_ind == 0)
+  //' @param lambda_d_breaks (J-1)-dim vector giving lambda[j] * (s[j] - s[j-1]), j = 1, ..., J-1
+  real pwe_lpdf(real y, real eta, vector lambda, vector log_lambda, vector breaks, int j, int death_ind, vector cumblhaz) {
+    real loglik;
+
+    // Initialize cumhaz to lambda[j] ( y - s[j] )
+    real cumhaz = lambda[j] * ( y - breaks[j] );
+
+    // add on (sum_{g=1}^{j-1} lambda[j] * ( s[j] - s[j-1] )
+    if ( j > 1 )
+      cumhaz += cumblhaz[j-1];
+
+    // Initialize loglik = - cumhaz * exp(x'beta) = log(Survival)
+    loglik = -cumhaz * exp(eta);
+
+    // If individual experienced event, add log hazard: log(lambda[j]) + x'beta
+    if ( death_ind == 1 )
+      loglik += log_lambda[j] + eta;
+
+    return(loglik);
+  }
+}
+data {
+  int<lower=0>                    n1;             // current data sample size
+  int<lower=0>                    n0;             // historical data sample size
+  int<lower=0>                    J;              // number of time intervals
+  int<lower=0>                    p;              // number of regression coefficients
+  vector[n1]                      y1;             // event times in current data
+  vector[n0]                      y0;             // event times in historical data
+  matrix[n1,p]                    X1;             // current data design matrix (no intercept for current data likelihood)
+  matrix[n0,p]                    X0;             // historical data design matrix (EXCLUDING intercept term)
+  array[n1] int<lower=1,upper=J>  intindx;        // index giving interval into which obs i failed / was censored for current data
+  array[n0] int<lower=1,upper=J>  intindx0;       // index giving interval into which obs i failed / was censored for historical data
+  array[n1] int<lower=0,upper=1>  death_ind;      // event indicator (1 = event; 0 = censored) for current data
+  array[n0] int<lower=0,upper=1>  death_ind0;     // event indicator (1 = event; 0 = censored) for historical data
+  vector[J+1]                     breaks;         // J+1-dim interval of breaks
+  vector[p]                       beta0_mean;     // mean for normal initial prior on coefficients in historical data model
+  vector<lower=0>[p]              beta0_sd;       // sd for normal initial prior on coefficients in historical data model
+  real<lower=0,upper=1>           p_spike;        // prior probability of spike
+  real                            mu_spike;       // location parameter for half-normal prior (spike)
+  real<lower=0>                   sigma_spike;    // scale parameter for half-normal prior (spike)
+  real                            mu_slab;        // location parameter for half-normal prior (slab)
+  real<lower=0>                   sigma_slab;     // scale parameter for half-normal prior (slab)
+  vector[J]                       hazard_mean;    // location parameter for half-normal prior on baseline hazards
+  vector<lower=0>[J]              hazard_sd;      // scale parameter for half-normal prior on baseline hazards
+  int<lower=0,upper=1>            get_loglik;     // whether to generate log-likelihood matrix
+}
+
+transformed data {
+  real lognc_spike   = normal_lccdf(0 | mu_spike, sigma_spike); // \Phi(mu_spike / sigma_spike)
+  real lognc_slab    = normal_lccdf(0 | mu_slab, sigma_slab);   // \Phi(mu_slab / sigma_slab)
+  real lognc_hazard  = normal_lccdf(0 | hazard_mean, hazard_sd);
+}
+parameters {
+  vector<lower=0>[J]    lambda;       // the J hazards for each interval for current data model
+  vector<lower=0>[J]    lambda0;      // the J hazards for each interval for historical data model
+  vector[p]             beta;
+  vector[p]             beta0;
+  vector<lower=0>[p]    comm_prec;    // commensurability parameter
+}
+transformed parameters{
+  vector[p] comm_sd = inv_sqrt(comm_prec);   // sd for commensurate prior
+}
+model {
+  vector[n1]  eta         = X1 * beta;
+  vector[n0]  eta0        = X0 * beta0;
+  vector[J]   log_lambda  = log(lambda);
+  vector[J]   log_lambda0 = log(lambda0);
+  vector[J-1] cumblhaz;
+  vector[J-1] cumblhaz0;
+
+  // compute lambda[j] * (s[j] * s[j-1])
+  cumblhaz = cumulative_sum( lambda[1:(J-1)] .* ( breaks[2:J] - breaks[1:(J-1)] ) );
+  cumblhaz0 = cumulative_sum( lambda0[1:(J-1)] .* ( breaks[2:J] - breaks[1:(J-1)] ) );
+
+  // half-normal prior on baseline hazards
+  target += normal_lpdf(lambda | hazard_mean, hazard_sd) - lognc_hazard;
+  target += normal_lpdf(lambda0 | hazard_mean, hazard_sd) - lognc_hazard;
+
+  // spike and slab prior for commensurability:
+  for ( i in 1:p ) {
+    target += log_mix(
+      p_spike
+      , normal_lpdf(comm_prec[i] | mu_spike, sigma_spike) - lognc_spike
+      , normal_lpdf(comm_prec[i] | mu_slab, sigma_slab) - lognc_slab
+    );
+  }
+
+  // initial prior for beta0
+  target += normal_lpdf(beta0 | beta0_mean, beta0_sd);
+  // commensurate prior for beta
+  target += normal_lpdf(beta  | beta0, comm_sd);
+
+  // historical data likelihood
+  for ( i in 1:n0 )
+    target += pwe_lpdf(y0[i] | eta0[i], lambda0, log_lambda0, breaks, intindx0[i], death_ind0[i], cumblhaz0);
+
+  // current data likelihood
+  for ( i in 1:n1 )
+    target += pwe_lpdf(y1[i] | eta[i], lambda, log_lambda, breaks, intindx[i], death_ind[i], cumblhaz);
+}
+generated quantities{
+  vector[(get_loglik == 1) ? n1 : 0] log_lik; //to generate log likelihood matrices
+  vector[n1]  eta        = X1 * beta;
+  vector[J]   log_lambda = log(lambda);
+  vector[J-1] cumblhaz   = cumulative_sum( lambda[1:(J-1)] .* ( breaks[2:J] - breaks[1:(J-1)] ) );
+
+  if ( get_loglik == 1 ) {
+    for( i in 1:n1 ){
+      log_lik[i] = pwe_lpdf(y1[i] | eta[i], lambda, log_lambda, breaks, intindx[i], death_ind[i], cumblhaz);
+    }
+  }
+}
+
