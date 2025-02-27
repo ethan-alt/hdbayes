6091e02b7460c3fe9c957631be74a8fe1c12d2fa
Xinxin Chen <xinxinchen476@gmail.com>
2025-02-27 14:57:40 -0500

    added functions for computing logml for NPP

---
+++ R/pwe_logml_npp.R
@@ -0,0 +1,182 @@
+#' Log marginal likelihood of a piecewise exponential (PWE) model under normalized power prior (NPP)
+#'
+#' Uses bridge sampling to estimate the logarithm of the marginal likelihood of a PWE model under the
+#' normalized power prior (NPP).
+#'
+#' @include pwe_loglik.R
+#' @include expfam_loglik.R
+#'
+#' @export
+#'
+#' @param post.samples      output from [pwe.npp()] giving posterior samples of a PWE model under the normalized
+#'                          power prior (NPP), with an attribute called 'data' which includes the list of variables
+#'                          specified in the data block of the Stan program.
+#' @param bridge.args       a `list` giving arguments (other than `samples`, `log_posterior`, `data`, `lb`, and `ub`) to
+#'                          pass onto [bridgesampling::bridge_sampler()].
+#'
+#' @return
+#'  The function returns a `list` with the following objects
+#'
+#'  \describe{
+#'    \item{model}{"NPP"}
+#'
+#'    \item{logml}{the estimated logarithm of the marginal likelihood}
+#'
+#'    \item{bs}{an object of class `bridge` or `bridge_list` containing the output from using [bridgesampling::bridge_sampler()]
+#'    to compute the logarithm of the marginal likelihood of the normalized power prior (NPP)}
+#'  }
+#'
+#' @references
+#'  Duan, Y., Ye, K., and Smith, E. P. (2005). Evaluating water quality using power priors to incorporate historical information. Environmetrics, 17(1), 95–106.
+#'
+#'  Gronau, Q. F., Singmann, H., and Wagenmakers, E.-J. (2020). bridgesampling: An r package for estimating normalizing constants. Journal of Statistical Software, 92(10).
+#'
+#' @examples
+#' \donttest{
+#'   if(requireNamespace("parallel")){
+#'     library(parallel)
+#'     ncores    = 2
+#'
+#'     if(requireNamespace("survival")){
+#'       library(survival)
+#'       data(E1684)
+#'       data(E1690)
+#'       ## take subset for speed purposes
+#'       E1684 = E1684[1:100, ]
+#'       E1690 = E1690[1:50, ]
+#'       ## replace 0 failure times with 0.50 days
+#'       E1684$failtime[E1684$failtime == 0] = 0.50/365.25
+#'       E1690$failtime[E1690$failtime == 0] = 0.50/365.25
+#'       E1684$cage = as.numeric(scale(E1684$age))
+#'       E1690$cage = as.numeric(scale(E1690$age))
+#'       data_list = list(currdata = E1690, histdata = E1684)
+#'       nbreaks = 3
+#'       probs   = 1:nbreaks / nbreaks
+#'       breaks  = as.numeric(
+#'         quantile(E1690[E1690$failcens==1, ]$failtime, probs = probs)
+#'       )
+#'       breaks  = c(0, breaks)
+#'       breaks[length(breaks)] = max(10000, 1000 * breaks[length(breaks)])
+#'       formula = survival::Surv(failtime, failcens) ~ treatment + sex + cage + node_bin
+#'     }
+#'
+#'     a0 = seq(0, 1, length.out = 11)
+#'     if (instantiate::stan_cmdstan_exists()) {
+#'       ## call created function
+#'       ## wrapper to obtain log normalizing constant in parallel package
+#'       logncfun = function(a0, ...){
+#'         hdbayes::pwe.npp.lognc(
+#'           formula = formula, histdata = data_list[[2]], breaks = breaks, a0 = a0,
+#'           ...
+#'         )
+#'       }
+#'
+#'       cl = makeCluster(ncores)
+#'       clusterSetRNGStream(cl, 123)
+#'       clusterExport(cl, varlist = c('formula', 'data_list', 'breaks'))
+#'       a0.lognc = parLapply(
+#'         cl = cl, X = a0, fun = logncfun, iter_warmup = 500,
+#'         iter_sampling = 1000, chains = 1, refresh = 0
+#'       )
+#'       stopCluster(cl)
+#'       a0.lognc = data.frame( do.call(rbind, a0.lognc) )
+#'
+#'       ## sample from normalized power prior
+#'       d.npp = pwe.npp(
+#'         formula = formula,
+#'         data.list = data_list,
+#'         a0.lognc = a0.lognc$a0,
+#'         lognc = a0.lognc$lognc,
+#'         breaks = breaks,
+#'         chains = 1, iter_warmup = 500, iter_sampling = 1000,
+#'         refresh = 0
+#'       )
+#'       pwe.logml.npp(
+#'         post.samples = d.npp,
+#'         bridge.args = list(silent = TRUE)
+#'       )
+#'     }
+#'   }
+#' }
+pwe.logml.npp = function(
+    post.samples,
+    bridge.args       = NULL
+) {
+  stan.data = attr(post.samples, 'data')
+  d         = as.matrix(post.samples)
+  ## rename parameters
+  p         = stan.data$p
+  X1        = stan.data$X1
+  J         = stan.data$J
+  oldnames = c(paste0("beta[", 1:p, "]"), paste0("lambda[", 1:J, "]"))
+  newnames = c(colnames(X1), paste0("basehaz[", 1:J, "]"))
+  colnames(d)[colnames(d) %in% newnames] = oldnames
+  oldnames  = c(oldnames, "logit_a0")
+  d = d[, oldnames, drop=F]
+
+  ## compute log normalizing constants (lognc) for half-normal prior on baseline hazards
+  stan.data$lognc_hazard = sum( pnorm(0, mean = stan.data$hazard_mean, sd = stan.data$hazard_sd, lower.tail = F, log.p = T) )
+
+  ## compute log normalizing constant for a0
+  a0_shape1       = stan.data$a0_shape1
+  a0_shape2       = stan.data$a0_shape2
+
+  stan.data$lognc_logit_a0 = 0
+  if( stan.data$a0_lower != 0 || stan.data$a0_upper != 1 ) {
+    stan.data$lognc_logit_a0 = log( pbeta(stan.data$a0_upper, shape1 = a0_shape1, shape2 = a0_shape2) -
+                                      pbeta(stan.data$a0_lower, shape1 = a0_shape1, shape2 = a0_shape2) )
+  }
+
+  ## log of the unnormalized posterior density function
+  log_density = function(pars, data){
+    a0_shape1  = data$a0_shape1
+    a0_shape2  = data$a0_shape2
+    a0_lower   = data$a0_lower
+    a0_upper   = data$a0_upper
+
+    beta       = as.numeric( pars[paste0("beta[", 1:data$p,"]")] )
+    lambda     = as.numeric( pars[paste0("lambda[", 1:data$J,"]")] )
+    prior_lp   = sum( dnorm(beta, mean = data$beta_mean, sd = data$beta_sd, log = T) ) +
+      sum( dnorm(lambda, mean = data$hazard_mean, sd = data$hazard_sd, log = T) ) - data$lognc_hazard
+
+    logit_a0   = as.numeric(pars["logit_a0"])
+    a0         = binomial('logit')$linkinv(logit_a0)
+    ## prior on logit(a0)
+    prior_lp   = prior_lp + logit_beta_lp(logit_a0, shape1 = a0_shape1, shape2 = a0_shape2) - data$lognc_logit_a0
+
+    eta     = data$X1 %*% beta
+    eta0    = data$X0 %*% beta
+    data_lp = a0 * sum( pwe_lpdf(data$y0, eta0, lambda, data$breaks, data$intindx0, data$J, data$death_ind0) ) +
+      sum( pwe_lpdf(data$y1, eta, lambda, data$breaks, data$intindx, data$J, data$death_ind) )
+
+    ## subtract log nc from power prior
+    prior_lp = prior_lp - pp_lognc(a0, data$a0_lognc, data$lognc)
+    return(data_lp + prior_lp)
+  }
+
+  lb        = c(rep(-Inf, p), rep(0, J), binomial('logit')$linkfun(stan.data$a0_lower))
+  ub        = c(rep(Inf, p+J), binomial('logit')$linkfun(stan.data$a0_upper))
+  names(ub) = colnames(d)
+  names(lb) = names(ub)
+
+  bs = do.call(
+    what = bridgesampling::bridge_sampler,
+    args = append(
+      list(
+        "samples" = d,
+        'log_posterior' = log_density,
+        'data' = stan.data,
+        'lb' = lb,
+        'ub' = ub),
+      bridge.args
+    )
+  )
+
+  ## Return a list of model name, estimated log marginal likelihood, and output from bridgesampling::bridge_sampler
+  res = list(
+    'model' = "NPP",
+    'logml' = bs$logml,
+    'bs'    = bs
+  )
+  return(res)
+}
---
+++ R/pwe_npp_lognc.R
@@ -0,0 +1,210 @@
+#' Estimate the logarithm of the normalizing constant for normalized power prior (NPP) for one data set
+#'
+#' Uses Markov chain Monte Carlo (MCMC) and bridge sampling to estimate the logarithm of the normalizing
+#' constant for the NPP for a fixed value of the power prior parameter \eqn{a_0 \in (0, 1)} for one data
+#' set. The initial priors are independent normal priors on the regression coefficients and half-normal
+#' priors on the baseline hazard parameters.
+#'
+#' @include data_checks_pwe.R
+#' @include pwe_pp_lognc.R
+#'
+#' @export
+#'
+#' @param formula           a two-sided formula giving the relationship between the response variable and covariates.
+#'                          The response is a survival object as returned by the `survival::Surv(time, event)` function,
+#'                          where event is a binary indicator for event (0 = no event, 1 = event has occurred). The type of
+#'                          censoring is assumed to be right-censoring.
+#' @param histdata          a `data.frame` giving the historical data.
+#' @param breaks            a numeric vector specifying the time points that define the boundaries of the piecewise
+#'                          intervals. The values should be in ascending order, with the final value being greater than
+#'                          or equal to the maximum observed time.
+#' @param a0                a scalar between 0 and 1 giving the (fixed) power prior parameter for the historical data.
+#' @param beta.mean         a scalar or a vector whose dimension is equal to the number of regression coefficients giving
+#'                          the mean parameters for the initial prior on regression coefficients. If a scalar is provided,
+#'                          `beta.mean` will be a vector of repeated elements of the given scalar. Defaults to a vector of 0s.
+#' @param beta.sd           a scalar or a vector whose dimension is equal to the number of regression coefficients giving
+#'                          the sd parameters for the initial prior on regression coefficients. If a scalar is provided,
+#'                          same as for `beta.mean`. Defaults to a vector of 10s.
+#' @param base.hazard.mean  a scalar or a vector whose dimension is equal to the number of intervals giving the location
+#'                          parameters for the half-normal priors on the baseline hazards of the PWE model. If a scalar is
+#'                          provided, same as for `beta.mean`. Defaults to 0.
+#' @param base.hazard.sd    a scalar or a vector whose dimension is equal to the number of intervals giving the scale
+#'                          parameters for the half-normal priors on the baseline hazards of the PWE model. If a scalar is
+#'                          provided, same as for `beta.mean`. Defaults to 10.
+#' @param bridge.args       a `list` giving arguments (other than `samples`, `log_posterior`, `data`, `lb`, and `ub`) to
+#'                          pass onto [bridgesampling::bridge_sampler()].
+#' @param iter_warmup       number of warmup iterations to run per chain. Defaults to 1000. See the argument `iter_warmup`
+#'                          in `sample()` method in cmdstanr package.
+#' @param iter_sampling     number of post-warmup iterations to run per chain. Defaults to 1000. See the argument `iter_sampling`
+#'                          in `sample()` method in cmdstanr package.
+#' @param chains            number of Markov chains to run. Defaults to 4. See the argument `chains` in `sample()` method
+#'                          in cmdstanr package.
+#' @param ...               arguments passed to `sample()` method in cmdstanr package (e.g., `seed`, `refresh`, `init`).
+#'
+#' @return
+#'  The function returns a vector giving the value of a0, the estimated logarithm of the normalizing constant, the minimum
+#'  estimated bulk effective sample size of the MCMC sampling, and the maximum Rhat.
+#'
+#' @references
+#'  Gronau, Q. F., Singmann, H., and Wagenmakers, E.-J. (2020). bridgesampling: An r package for estimating normalizing constants. Journal of Statistical Software, 92(10).
+#'
+#' @examples
+#' if (instantiate::stan_cmdstan_exists()) {
+#'   if(requireNamespace("survival")){
+#'     library(survival)
+#'     data(E1684)
+#'     ## take subset for speed purposes
+#'     E1684 = E1684[1:100, ]
+#'     ## replace 0 failure times with 0.50 days
+#'     E1684$failtime[E1684$failtime == 0] = 0.50/365.25
+#'     E1684$cage = as.numeric(scale(E1684$age))
+#'     nbreaks = 3
+#'     probs   = 1:nbreaks / nbreaks
+#'     breaks  = as.numeric(
+#'       quantile(E1684[E1684$failcens==1, ]$failtime, probs = probs)
+#'     )
+#'     breaks  = c(0, breaks)
+#'     breaks[length(breaks)] = max(10000, 1000 * breaks[length(breaks)])
+#'     pwe.npp.lognc(
+#'       formula = survival::Surv(failtime, failcens) ~ treatment + sex + cage + node_bin,
+#'       histdata = E1684,
+#'       breaks = breaks,
+#'       a0 = 0.5,
+#'       bridge.args = list(silent = TRUE),
+#'       chains = 1, iter_warmup = 500, iter_sampling = 1000
+#'     )
+#'   }
+#' }
+pwe.npp.lognc = function(
+    formula,
+    histdata,
+    breaks,
+    a0,
+    beta.mean         = NULL,
+    beta.sd           = NULL,
+    base.hazard.mean  = NULL,
+    base.hazard.sd    = NULL,
+    bridge.args       = NULL,
+    iter_warmup       = 1000,
+    iter_sampling     = 1000,
+    chains            = 4,
+    ...
+) {
+  data.checks.pwe(formula, list(histdata), breaks)
+
+  ## extract names and variables for response, censoring, etc.
+  time.name     = all.vars(formula)[1]
+  eventind.name = all.vars(formula)[2]
+  y0            = histdata[, time.name]
+  eventind0     = as.integer( histdata[, eventind.name] )
+  X0            = stats::model.matrix(formula, histdata)
+  if ( '(Intercept)' %in% colnames(X0) )
+    X0 = X0[, -1, drop = F]
+
+  p = ncol(X0)
+  J = length(breaks) - 1  ## number of intervals
+
+  ## create index giving interval into which obs failed / was censored
+  intindx0 = sapply(y0, function(t){
+    if( t == 0 ){
+      return(1)
+    }else{
+      return(findInterval(t, breaks, left.open = TRUE))
+    }
+  })
+
+  ## Default prior on regression coefficients is N(0, 10^2)
+  if ( !is.null(beta.mean) ){
+    if ( !( is.vector(beta.mean) & (length(beta.mean) %in% c(1, p)) ) )
+      stop("beta.mean must be a scalar or a vector of length ", p, " if beta.mean is not NULL")
+  }
+  beta.mean = to.vector(param = beta.mean, default.value = 0, len = p)
+  if ( !is.null(beta.sd) ){
+    if ( !( is.vector(beta.sd) & (length(beta.sd) %in% c(1, p)) ) )
+      stop("beta.sd must be a scalar or a vector of length ", p, " if beta.sd is not NULL")
+  }
+  beta.sd = to.vector(param = beta.sd, default.value = 10, len = p)
+
+  ## Default half-normal priors on baseline hazards are N^{+}(0, 10^2)
+  if ( !is.null(base.hazard.mean) ){
+    if ( !( is.vector(base.hazard.mean) & (length(base.hazard.mean) %in% c(1, J)) ) )
+      stop("base.hazard.mean must be a scalar or a vector of length ", J, " if base.hazard.mean is not NULL")
+  }
+  base.hazard.mean = to.vector(param = base.hazard.mean, default.value = 0, len = J)
+  if ( !is.null(base.hazard.sd) ){
+    if ( !( is.vector(base.hazard.sd) & (length(base.hazard.sd) %in% c(1, J)) ) )
+      stop("base.hazard.sd must be a scalar or a vector of length ", J, " if base.hazard.sd is not NULL")
+  }
+  base.hazard.sd = to.vector(param = base.hazard.sd, default.value = 10, len = J)
+
+  ## check a0 value
+  a0 = as.numeric(a0)
+  if ( any(a0 < 0 | a0 > 1 ) )
+    stop("a0 must be a scalar between 0 and 1")
+
+  standat = list(
+    'n0'              = nrow(X0),
+    'J'               = J,
+    'p'               = p,
+    'y0'              = y0,
+    'X0'              = X0,
+    'intindx0'        = intindx0,
+    'death_ind0'      = eventind0,
+    'breaks'          = breaks,
+    'a0'              = a0,
+    'beta_mean'       = beta.mean,
+    'beta_sd'         = beta.sd,
+    'hazard_mean'     = base.hazard.mean,
+    'hazard_sd'       = base.hazard.sd
+  )
+
+  pwe_pp_prior = instantiate::stan_package_model(
+    name = "pwe_pp_prior",
+    package = "hdbayes"
+  )
+
+  ## fit model in cmdstanr
+  fit = pwe_pp_prior$sample(data = standat,
+                            iter_warmup = iter_warmup, iter_sampling = iter_sampling, chains = chains,
+                            ...)
+  d    = fit$draws(format = 'draws_df')
+  attr(x = d, which = 'data') = standat
+  summ = posterior::summarise_draws(d)
+
+  ## compute log normalizing constant
+  res.hist = pwe.pp.lognc(
+    post.samples   = d,
+    is.prior       = TRUE,
+    bridge.args    = bridge.args
+  )
+
+  ## Return vector of a0, lognc, min_n_eff, max_Rhat
+  res = c(
+    'a0'           = a0,
+    'lognc'        = res.hist$lognc,
+    'min_ess_bulk' = min(summ[, 'ess_bulk']),
+    'max_Rhat'     = max(summ[, 'rhat'])
+  )
+
+  if ( res['min_ess_bulk'] < 1000 )
+    warning(
+      paste0(
+        'The minimum bulk effective sample size of the MCMC sampling is ',
+        res['min_ess_bulk'],
+        ' for a0 = ',
+        round(a0, 4),
+        '. It is recommended to have at least 1000. Try increasing the number of iterations.'
+      )
+    )
+  if ( res['max_Rhat'] > 1.10 )
+    warning(
+      paste0(
+        'The maximum Rhat of the MCMC sampling is ',
+        res['max_Rhat'],
+        ' for a0 = ',
+        round(a0, 4),
+        '. It is recommended to have a maximum Rhat of no more than 1.1. Try increasing the number of iterations.'
+      )
+    )
+  return(res)
+}
