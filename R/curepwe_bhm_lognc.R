f8e729cc7f638cf0a599b7551c617fd0466e5286
Xinxin Chen <xinxinchen476@gmail.com>
2025-03-06 15:56:50 -0500

    added functions for computing logml for BHM

---
+++ R/curepwe_bhm_lognc.R
@@ -0,0 +1,179 @@
+#' Estimate the logarithm of the normalizing constant for Bayesian hierarchical model (BHM)
+#'
+#' Uses bridge sampling to estimate the logarithm of the normalizing constant for Bayesian hierarchical
+#' model (BHM) using all data sets or using historical data set only.
+#'
+#' @include pwe_loglik.R
+#' @include mixture_loglik.R
+#'
+#' @noRd
+#'
+#' @param post.samples      posterior samples of a CurePWE model under the Bayesian hierarchical model (BHM) or samples from
+#'                          the prior induced by the BHM, with an attribute called 'data' which includes the list of variables
+#'                          specified in the data block of the Stan program.
+#' @param is.prior          whether the samples are from the prior induced by the BHM (using historical data set only).
+#'                          Defaults to FALSE.
+#' @param bridge.args       a `list` giving arguments (other than `samples`, `log_posterior`, `data`, `lb`, and `ub`)
+#'                          to pass onto [bridgesampling::bridge_sampler()].
+#'
+#' @return
+#'  The function returns a `list` with the following objects
+#'
+#'  \describe{
+#'    \item{lognc}{the estimated logarithm of the normalizing constant}
+#'
+#'    \item{bs}{an object of class `bridge` or `bridge_list` giving the output from [bridgesampling::bridge_sampler()]}
+#'  }
+#'
+#' @references
+#'  Gronau, Q. F., Singmann, H., and Wagenmakers, E.-J. (2020). bridgesampling: An r package for estimating normalizing constants. Journal of Statistical Software, 92(10).
+#'
+#' @examples
+#' if (instantiate::stan_cmdstan_exists()) {
+#'   if(requireNamespace("survival")){
+#'     library(survival)
+#'     data(E1684)
+#'     data(E1690)
+#'     ## take subset for speed purposes
+#'     E1684 = E1684[1:100, ]
+#'     E1690 = E1690[1:50, ]
+#'     ## replace 0 failure times with 0.50 days
+#'     E1684$failtime[E1684$failtime == 0] = 0.50/365.25
+#'     E1690$failtime[E1690$failtime == 0] = 0.50/365.25
+#'     E1684$cage = as.numeric(scale(E1684$age))
+#'     E1690$cage = as.numeric(scale(E1690$age))
+#'     data_list = list(currdata = E1690, histdata = E1684)
+#'     nbreaks = 3
+#'     probs   = 1:nbreaks / nbreaks
+#'     breaks  = as.numeric(
+#'       quantile(E1690[E1690$failcens==1, ]$failtime, probs = probs)
+#'     )
+#'     breaks  = c(0, breaks)
+#'     breaks[length(breaks)] = max(10000, 1000 * breaks[length(breaks)])
+#'     d.bhm = curepwe.bhm(
+#'       formula = survival::Surv(failtime, failcens) ~ treatment + sex + cage + node_bin,
+#'       data.list = data_list,
+#'       breaks = breaks,
+#'       chains = 1, iter_warmup = 500, iter_sampling = 1000
+#'     )
+#'     curepwe.bhm.lognc(
+#'       post.samples = d.bhm,
+#'       is.prior = FALSE,
+#'       bridge.args = list(silent = TRUE)
+#'     )
+#'   }
+#' }
+curepwe.bhm.lognc = function(
+    post.samples,
+    is.prior          = FALSE,
+    bridge.args       = NULL
+) {
+  ## get Stan data for BHM
+  stan.data = attr(post.samples, 'data')
+  d         = as.matrix(post.samples)
+
+  ## rename parameters
+  p        = stan.data$p
+  J        = stan.data$J
+
+  if( is.prior ){
+    oldnames = c(paste0("lambda0[", 1:J, "]"), paste0('beta0_raw[', 1:p, ']'),
+                 "logit_p_cured0")
+  }else{
+    oldnames = c(paste0("lambda[", 1:J, "]"), paste0("lambda0[", 1:J, "]"))
+    newnames = c(paste0("basehaz[", 1:J, "]"), paste0("basehaz_hist[", 1:J, "]"))
+    colnames(d)[colnames(d) %in% newnames] = oldnames
+
+    oldnames = c(oldnames, paste0('beta_raw[', 1:p, ']'), paste0('beta0_raw[', 1:p, ']'),
+                 "logit_p_cured", "logit_p_cured0")
+  }
+  oldnames = c(oldnames, paste0('beta_mean[', 1:p, ']'), paste0('beta_sd[', 1:p, ']'))
+  d = d[, oldnames, drop=F]
+
+  ## compute log normalizing constants for half-normal priors
+  stan.data$lognc_hazard      = sum( pnorm(0, mean = stan.data$hazard_mean, sd = stan.data$hazard_sd, lower.tail = F, log.p = T) )
+  stan.data$lognc_beta_sd     = sum( pnorm(0, mean = stan.data$meta_sd_mean, sd = stan.data$meta_sd_sd, lower.tail = F, log.p = T) )
+  stan.data$is_prior          = is.prior
+
+  ## log of the unnormalized posterior density function
+  log_density = function(pars, data){
+    p          = data$p
+    beta0_raw  = as.numeric( pars[paste0('beta0_raw[', 1:p, ']')] )
+    beta_mean  = as.numeric( pars[paste0('beta_mean[', 1:p, ']')] )
+    beta_sd    = as.numeric( pars[paste0('beta_sd[', 1:p, ']')] )
+    lambda0    = as.numeric( pars[paste0("lambda0[", 1:data$J,"]")] )
+
+    logit_p_cured0 = as.numeric( pars[["logit_p_cured0"]] )
+    log1m_p_cured0 = -log1p_exp(logit_p_cured0) # log(1 - p_cured0)
+    log_probs0     = c(logit_p_cured0, 0) + log1m_p_cured0 # c(log(p_cured0), log(1 - p_cured0))
+
+    ## prior on beta_mean and beta_sd
+    prior_lp   = sum( dnorm(beta_mean, mean = data$meta_mean_mean, sd = data$meta_mean_sd, log = T) ) +
+      sum( dnorm(beta_sd, mean = data$meta_sd_mean, sd = data$meta_sd_sd, log = T) ) - data$lognc_beta_sd
+
+    ## prior on beta0_raw (equivalent to prior on beta0)
+    prior_lp   = prior_lp + sum( dnorm(beta0_raw, mean = 0, sd = 1, log = T) )
+
+    ## priors on lambda0 and logit_p_cured0
+    prior_lp   = prior_lp + sum( dnorm(lambda0, mean = data$hazard_mean, sd = data$hazard_sd, log = T) ) - data$lognc_hazard +
+      dnorm(logit_p_cured0, mean = data$logit_p_cured_mean, sd = data$logit_p_cured_sd, log = T)
+
+    beta0      = beta_mean + beta0_raw * beta_sd
+    eta0       = data$X0 %*% beta0
+    contribs0  = cbind(log_probs0[1] + log(1 - data$death_ind0),
+                      log_probs0[2] + pwe_lpdf(data$y0, eta0, lambda0, data$breaks, data$intindx0, data$J, data$death_ind0))
+    data_lp    = sum( apply(contribs0, 1, log_sum_exp) )
+
+    if( !data$is_prior ){
+      beta_raw   = as.numeric( pars[paste0('beta_raw[', 1:p, ']')] )
+      lambda     = as.numeric( pars[paste0("lambda[", 1:data$J,"]")] )
+
+      logit_p_cured = as.numeric( pars[["logit_p_cured"]] )
+      log1m_p_cured = -log1p_exp(logit_p_cured) # log(1 - p_cured)
+      log_probs     = c(logit_p_cured, 0) + log1m_p_cured # c(log(p_cured), log(1 - p_cured))
+
+      ## prior on beta_raw (equivalent to prior on beta)
+      prior_lp   = prior_lp + sum( dnorm(beta_raw, mean = 0, sd = 1, log = T) )
+
+      ## priors on lambda and logit_p_cured
+      prior_lp   = prior_lp + sum( dnorm(lambda, mean = data$hazard_mean, sd = data$hazard_sd, log = T) ) - data$lognc_hazard +
+        dnorm(logit_p_cured, mean = data$logit_p_cured_mean, sd = data$logit_p_cured_sd, log = T)
+
+      beta       = beta_mean + beta_raw * beta_sd
+      eta        = data$X1 %*% beta
+      contribs   = cbind(log_probs[1] + log(1 - data$death_ind),
+                       log_probs[2] + pwe_lpdf(data$y1, eta, lambda, data$breaks, data$intindx, data$J, data$death_ind))
+      data_lp    = data_lp + sum( apply(contribs, 1, log_sum_exp) )
+    }
+    return(data_lp + prior_lp)
+  }
+
+  if( is.prior ){
+    lb           = c(rep(0, J), rep(-Inf, 1+2*p), rep(0, p))
+  }else{
+    lb           = c(rep(0, 2*J), rep(-Inf, 2+3*p), rep(0, p))
+  }
+  ub           = rep(Inf, length(lb))
+  names(ub)    = colnames(d)
+  names(lb)    = names(ub)
+
+  bs = do.call(
+    what = bridgesampling::bridge_sampler,
+    args = append(
+      list(
+        "samples" = d,
+        'log_posterior' = log_density,
+        'data' = stan.data,
+        'lb' = lb,
+        'ub' = ub),
+      bridge.args
+    )
+  )
+
+  ## Return a list of lognc and output from bridgesampling::bridge_sampler
+  res = list(
+    'lognc'        = bs$logml,
+    'bs'           = bs
+  )
+  return(res)
+}
---
+++ R/curepwe_logml_map.R
@@ -0,0 +1,152 @@
+#' Log marginal likelihood of a standard cure rate (CurePWE) model under the meta-analytic predictive (MAP) prior
+#'
+#' @description Uses Markov chain Monte Carlo (MCMC) and bridge sampling to estimate the logarithm of the marginal
+#' likelihood of a CurePWE model under the meta-analytic predictive (MAP) prior. The MAP prior is equivalent to the prior
+#' induced by the Bayesian hierarchical model (BHM).
+#'
+#' @description The arguments related to MCMC sampling are utilized to draw samples from the MAP prior. These
+#' samples are then used to compute the logarithm of the normalizing constant of the MAP prior using only historical
+#' data set.
+#'
+#' @include curepwe_bhm_lognc.R
+#'
+#' @export
+#'
+#' @param post.samples      output from [curepwe.bhm()] giving posterior samples of a CurePWE model under the Bayesian
+#'                          hierarchical model (BHM), with an attribute called 'data' which includes the list of variables
+#'                          specified in the data block of the Stan program.
+#' @param bridge.args       a `list` giving arguments (other than `samples`, `log_posterior`, `data`, `lb`, and `ub`) to
+#'                          pass onto [bridgesampling::bridge_sampler()].
+#' @param iter_warmup       number of warmup iterations to run per chain. Defaults to 1000. See the argument `iter_warmup`
+#'                          in `sample()` method in cmdstanr package.
+#' @param iter_sampling     number of post-warmup iterations to run per chain. Defaults to 1000. See the argument `iter_sampling`
+#'                          in `sample()` method in cmdstanr package.
+#' @param chains            number of Markov chains to run. Defaults to 4. See the argument `chains` in `sample()` method
+#'                          in cmdstanr package.
+#' @param ...               arguments passed to `sample()` method in cmdstanr package (e.g., `seed`, `refresh`, `init`).
+#'
+#' @return
+#'  The function returns a `list` with the following objects
+#'
+#'  \describe{
+#'    \item{model}{"MAP"}
+#'
+#'    \item{logml}{the estimated logarithm of the marginal likelihood}
+#'
+#'    \item{bs}{an object of class `bridge` or `bridge_list` containing the output from using [bridgesampling::bridge_sampler()]
+#'    to compute the logarithm of the normalizing constant of the Bayesian hierarchical model (BHM) using all data sets}
+#'
+#'    \item{bs.hist}{an object of class `bridge` or `bridge_list` containing the output from using
+#'    [bridgesampling::bridge_sampler()] to compute the logarithm of the normalizing constant of the BHM using historical
+#'    data set}
+#'
+#'    \item{min_ess_bulk}{the minimum estimated bulk effective sample size of the MCMC sampling}
+#'
+#'    \item{max_Rhat}{the maximum Rhat}
+#'  }
+#'
+#' @references
+#'  Gronau, Q. F., Singmann, H., and Wagenmakers, E.-J. (2020). bridgesampling: An r package for estimating normalizing constants. Journal of Statistical Software, 92(10).
+#'
+#' @examples
+#' if (instantiate::stan_cmdstan_exists()) {
+#'   if(requireNamespace("survival")){
+#'     library(survival)
+#'     data(E1684)
+#'     data(E1690)
+#'     ## take subset for speed purposes
+#'     E1684 = E1684[1:100, ]
+#'     E1690 = E1690[1:50, ]
+#'     ## replace 0 failure times with 0.50 days
+#'     E1684$failtime[E1684$failtime == 0] = 0.50/365.25
+#'     E1690$failtime[E1690$failtime == 0] = 0.50/365.25
+#'     E1684$cage = as.numeric(scale(E1684$age))
+#'     E1690$cage = as.numeric(scale(E1690$age))
+#'     data_list = list(currdata = E1690, histdata = E1684)
+#'     nbreaks = 3
+#'     probs   = 1:nbreaks / nbreaks
+#'     breaks  = as.numeric(
+#'       quantile(E1690[E1690$failcens==1, ]$failtime, probs = probs)
+#'     )
+#'     breaks  = c(0, breaks)
+#'     breaks[length(breaks)] = max(10000, 1000 * breaks[length(breaks)])
+#'     d.bhm = curepwe.bhm(
+#'       formula = survival::Surv(failtime, failcens) ~ treatment + sex + cage + node_bin,
+#'       data.list = data_list,
+#'       breaks = breaks,
+#'       chains = 1, iter_warmup = 500, iter_sampling = 1000
+#'     )
+#'     curepwe.logml.map(
+#'       post.samples = d.bhm,
+#'       bridge.args = list(silent = TRUE),
+#'       chains = 1, iter_warmup = 1000, iter_sampling = 2000
+#'     )
+#'   }
+#' }
+curepwe.logml.map = function(
+    post.samples,
+    bridge.args       = NULL,
+    iter_warmup       = 1000,
+    iter_sampling     = 1000,
+    chains            = 4,
+    ...
+) {
+  stan.data = attr(post.samples, 'data')
+
+  ## computing log normalizing constant for the prior induced by BHM using all data sets
+  res.all = curepwe.bhm.lognc(
+    post.samples   = post.samples,
+    is.prior       = FALSE,
+    bridge.args    = bridge.args
+  )
+
+  ## sample from the prior induced by BHM
+  curepwe_bhm_prior = instantiate::stan_package_model(
+    name = "curepwe_bhm_prior",
+    package = "hdbayes"
+  )
+
+  fit = curepwe_bhm_prior$sample(data = stan.data,
+                                 iter_warmup = iter_warmup, iter_sampling = iter_sampling, chains = chains,
+                                 ...)
+  summ = posterior::summarise_draws(fit)
+
+  hist.post.samples = fit$draws(format = 'draws_df')
+  attr(x = hist.post.samples, which = 'data') = stan.data
+
+  ## compute log normalizing constant for the prior induced by BHM using historical data set
+  res.hist = curepwe.bhm.lognc(
+    post.samples   = hist.post.samples,
+    is.prior       = TRUE,
+    bridge.args    = bridge.args
+  )
+
+  ## Return a list of model name, estimated log marginal likelihood, outputs from bridgesampling::bridge_sampler,
+  ## the minimum estimated bulk effective sample size of the MCMC sampling, and the maximum Rhat
+  res = list(
+    'model'        = "MAP",
+    'logml'        = res.all$lognc - res.hist$lognc,
+    'bs'           = res.all$bs,
+    'bs.hist'      = res.hist$bs,
+    'min_ess_bulk' = min(summ[, 'ess_bulk']),
+    'max_Rhat'     = max(summ[, 'rhat'])
+  )
+
+  if ( res[['min_ess_bulk']] < 1000 )
+    warning(
+      paste0(
+        'The minimum bulk effective sample size of the MCMC sampling is ',
+        round(res[['min_ess_bulk']], 4),
+        '. It is recommended to have at least 1000. Try increasing the number of iterations.'
+      )
+    )
+  if ( res[['max_Rhat']] > 1.10 )
+    warning(
+      paste0(
+        'The maximum Rhat of the MCMC sampling is ',
+        round(res[['max_Rhat']], 4),
+        '. It is recommended to have a maximum Rhat of no more than 1.1. Try increasing the number of iterations.'
+      )
+    )
+  return(res)
+}
