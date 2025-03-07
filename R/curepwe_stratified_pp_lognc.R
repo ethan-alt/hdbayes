dda55eb11453bb5e0ad99e6a0486214daca00cac
Xinxin Chen <xinxinchen476@gmail.com>
2025-03-07 10:33:49 -0500

    added functions for computing logml of a CurePWE model under stratified PP

---
+++ R/curepwe_stratified_pp_lognc.R
@@ -0,0 +1,173 @@
+#' Estimate the logarithm of the normalizing constant for stratified power prior (PP)
+#'
+#' Uses bridge sampling to estimate the logarithm of the normalizing constant for the stratified power
+#' prior (PP) using all data sets or using historical data set only. Note that the power prior parameters
+#' (\eqn{a_0}'s) are treated as fixed.
+#'
+#' @include pwe_loglik.R
+#' @include mixture_loglik.R
+#'
+#' @noRd
+#'
+#' @param post.samples      posterior samples of a CurePWE model under the stratified power prior (PP) or samples from
+#'                          the stratified PP, with an attribute called 'data' which includes the list of variables
+#'                          specified in the data block of the Stan program.
+#' @param is.prior          whether the samples are from the stratified PP (using historical data sets only).
+#'                          Defaults to FALSE.
+#' @param bridge.args       a `list` giving arguments (other than `samples`, `log_posterior`, `data`, `lb`, and `ub`)
+#'                          to pass onto [bridgesampling::bridge_sampler()].
+#'
+#' @return
+#'  The function returns a `list` with the following objects
+#'
+#'  \describe{
+#'    \item{lognc}{the estimated logarithm of the normalizing constant}
+#'
+#'    \item{bs}{an object of class `bridge` or `bridge_list` giving the output from [bridgesampling::bridge_sampler()]}
+#'  }
+#'
+#' @references
+#'  Wang, C., Li, H., Chen, W.-C., Lu, N., Tiwari, R., Xu, Y., & Yue, L. Q. (2019). Propensity score-integrated power prior approach for incorporating real-world evidence in single-arm clinical studies. Journal of Biopharmaceutical Statistics, 29(5), 731–748.
+#'
+#'  Gronau, Q. F., Singmann, H., and Wagenmakers, E.-J. (2020). bridgesampling: An r package for estimating normalizing constants. Journal of Statistical Software, 92(10).
+#'
+#' @examples
+#' if (instantiate::stan_cmdstan_exists()) {
+#'   if(requireNamespace("survival")){
+#'     library(survival)
+#'     data(E1684)
+#'     data(E1690)
+#'     ## take subset for speed purposes
+#'     E1684 = E1684[1:100, ]
+#'     E1690 = E1690[1:50, ]
+#'     ## replace 0 failure times with 0.50 days
+#'     E1684$failtime[E1684$failtime == 0] = 0.50/365.25
+#'     E1690$failtime[E1690$failtime == 0] = 0.50/365.25
+#'     data_list = list(currdata = E1690, histdata = E1684)
+#'     strata_list = list(rep(1:2, each = 25), rep(1:2, each = 50))
+#'     # Alternatively, we can determine the strata based on propensity scores
+#'     # using the psrwe package, which is available on GitHub
+#'     nbreaks = 3
+#'     probs   = 1:nbreaks / nbreaks
+#'     breaks  = as.numeric(
+#'       quantile(E1690[E1690$failcens==1, ]$failtime, probs = probs)
+#'     )
+#'     breaks  = c(0, breaks)
+#'     breaks[length(breaks)] = max(10000, 1000 * breaks[length(breaks)])
+#'     d.stratified.pp = curepwe.stratified.pp(
+#'       formula = survival::Surv(failtime, failcens) ~ treatment,
+#'       data.list = data_list,
+#'       strata.list = strata_list,
+#'       breaks = breaks,
+#'       a0.strata = c(0.3, 0.5),
+#'       chains = 1, iter_warmup = 500, iter_sampling = 1000
+#'     )
+#'     curepwe.stratified.pp.lognc(
+#'       post.samples = d.stratified.pp,
+#'       is.prior = FALSE,
+#'       bridge.args = list(silent = TRUE)
+#'     )
+#'   }
+#' }
+curepwe.stratified.pp.lognc = function(
+    post.samples,
+    is.prior          = FALSE,
+    bridge.args       = NULL
+) {
+  stan.data = attr(post.samples, 'data')
+  d         = as.matrix(post.samples)
+  ## rename parameters
+  p         = stan.data$p
+  X0        = stan.data$X0
+  J         = stan.data$J
+  K         = stan.data$K
+  oldnames = c(paste0("betaMat[", rep(1:p, K), ',', rep(1:K, each = p), "]"),
+               paste0("lambdaMat[", rep(1:J, K), ',', rep(1:K, each = J), "]"))
+  newnames = c(paste0( colnames(X0), '_stratum_', rep(1:K, each = p) ),
+               paste0("basehaz", "_stratum_", rep(1:K, each = J), "[", 1:J, "]"))
+  colnames(d)[colnames(d) %in% newnames] = oldnames
+  oldnames = c(oldnames, paste0("logit_p_curedVec[", 1:K, "]"))
+  d = d[, oldnames, drop=F]
+
+  ## compute log normalizing constants (lognc) for half-normal prior on baseline hazards
+  stan.data$lognc_hazard = sum( pnorm(0, mean = stan.data$hazard_mean, sd = stan.data$hazard_sd, lower.tail = F, log.p = T) )
+  stan.data$is_prior     = is.prior
+
+  ## log of the unnormalized posterior density function
+  log_density = function(pars, data){
+    p          = data$p
+    K          = data$K
+    beta       = pars[paste0("betaMat[", rep(1:p, K), ',', rep(1:K, each = p), "]")]
+    beta       = matrix(beta, nrow = p, ncol = K)
+    lambda     = pars[paste0("lambdaMat[", rep(1:J, K), ',', rep(1:K, each = J), "]")]
+    lambda     = matrix(lambda, nrow = J, ncol = K)
+    prior_lp   = sum( sapply(1:K, function(k){
+      sum(dnorm(beta[, k], mean = data$beta_mean, sd = data$beta_sd, log = T)) +
+        sum(dnorm(lambda[, k], mean = data$hazard_mean, sd = data$hazard_sd, log = T)) - data$lognc_hazard
+    }) )
+
+    logit_p_curedVec = as.numeric( pars[paste0("logit_p_curedVec[", 1:K, "]")] )
+    log_probs        = sapply(1:K, function(k){
+      log1m_p_cured = -log1p_exp(logit_p_curedVec[k]) # log(1 - p_cured)
+      return(c(logit_p_curedVec[k], 0) + log1m_p_cured) # c(log(p_cured), log(1 - p_cured))
+    })
+    log_probs        = as.matrix(log_probs, ncol = K)
+
+    ## prior on logit_p_curedVec
+    prior_lp = prior_lp + sum( dnorm(logit_p_curedVec, mean = data$logit_p_cured_mean, sd = data$logit_p_cured_sd, log = T) )
+
+    Eta0       = data$X0 %*% beta
+    stratumID0 = data$stratumID0
+    a0s        = data$a0s
+    y0         = data$y0
+    data_lp    = sapply(1:data$n0, function(i){
+      a0s[ stratumID0[i] ] * log_sum_exp(
+        c(log_probs[1, stratumID0[i]] + log(1 - data$death_ind0[i]),
+          log_probs[2, stratumID0[i]] +
+            pwe_lpdf(y0[i], Eta0[i, stratumID0[i]], lambda[, stratumID0[i]], data$breaks, data$intindx0[i], data$J, data$death_ind0[i])
+          )
+      )
+    })
+    data_lp    = sum(data_lp)
+
+    if( !data$is_prior ){
+      Eta       = data$X1 %*% beta
+      stratumID = data$stratumID
+      y1        = data$y1
+      data_lp = data_lp + sum( sapply(1:data$n1, function(i){
+        log_sum_exp(
+          c(log_probs[1, stratumID[i]] + log(1 - data$death_ind[i]),
+            log_probs[2, stratumID[i]] +
+              pwe_lpdf(y1[i], Eta[i, stratumID[i]], lambda[, stratumID[i]], data$breaks, data$intindx[i], data$J, data$death_ind[i])
+          )
+        )
+      }) )
+    }
+    return(data_lp + prior_lp)
+  }
+
+  lb           = c(rep(-Inf, p*K), rep(0, J*K), rep(-Inf, K))
+  ub           = rep(Inf, length(lb))
+  names(ub)    = colnames(d)
+  names(lb)    = names(ub)
+
+  bs = do.call(
+    what = bridgesampling::bridge_sampler,
+    args = append(
+      list(
+        "samples" = d,
+        'log_posterior' = log_density,
+        'data' = stan.data,
+        'lb' = lb,
+        'ub' = ub),
+      bridge.args
+    )
+  )
+
+  ## Return a list of lognc and output from bridgesampling::bridge_sampler
+  res = list(
+    'lognc'        = bs$logml,
+    'bs'           = bs
+  )
+  return(res)
+}
---
+++ src/stan/curepwe_stratified_pp.stan
@@ -0,0 +1,136 @@
+// fit standard cure rate model: S(t) = pcure + (1 - pcure) * survival function for PWE model
+functions {
+  //' PWE log likelihood
+  //' @param y               failure / censoring time
+  //' @param eta             linear predictor
+  //' @param lambda          vector of baseline hazards
+  //' @param log_lambda      vector of log(lambda)
+  //' @param breaks          (J+1)-dim vector giving intervals
+  //' @param j               index of the interval for which the individual failed / was censored: j \in {1, ..., J}
+  //' @param death_ind       integer giving whether individual died (death_ind == 1) or was censored (death_ind == 0)
+  //' @param lambda_d_breaks (J-1)-dim vector giving lambda[j] * (s[j] - s[j-1]), j = 1, ..., J-1
+  real pwe_lpdf(real y, real eta, vector lambda, vector log_lambda, vector breaks, int j, int death_ind, vector cumblhaz) {
+    real loglik;
+
+    // Initialize cumhaz to lambda[j] ( y - s[j] )
+    real cumhaz = lambda[j] * ( y - breaks[j] );
+
+    // add on (sum_{g=1}^{j-1} lambda[j] * ( s[j] - s[j-1] )
+    if ( j > 1 )
+      cumhaz += cumblhaz[j-1];
+
+    // Initialize loglik = - cumhaz * exp(x'beta) = log(Survival)
+    loglik = -cumhaz * exp(eta);
+
+    // If individual experienced event, add log hazard: log(lambda[j]) + x'beta
+    if ( death_ind == 1 )
+      loglik += log_lambda[j] + eta;
+
+    return(loglik);
+  }
+}
+data {
+  int<lower=0>                    n1;            // current data sample size
+  int<lower=0>                    n0;            // historical data sample size
+  int<lower=0>                    J;             // number of time intervals
+  int<lower=0>                    p;             // number of regression coefficients
+  vector[n1]                      y1;            // event times in current data
+  vector[n0]                      y0;            // event times in historical data
+  matrix[n1,p]                    X1;            // current data design matrix (no intercept for current data likelihood)
+  matrix[n0,p]                    X0;            // historical data design matrix (EXCLUDING intercept term)
+  array[n1] int<lower=1,upper=J>  intindx;       // index giving interval into which obs i failed / was censored for current data
+  array[n0] int<lower=1,upper=J>  intindx0;      // index giving interval into which obs i failed / was censored for historical data
+  array[n1] int<lower=0,upper=1>  death_ind;     // event indicator (1 = event; 0 = censored) for current data
+  array[n0] int<lower=0,upper=1>  death_ind0;    // event indicator (1 = event; 0 = censored) for historical data
+  vector[J+1]                     breaks;        // J+1-dim interval of breaks
+  int<lower = 0>                  K;             // number of strata
+  array[n1] int<lower=1,upper=K>  stratumID;     // strata assignment for current data
+  array[n0] int<lower=1,upper=K>  stratumID0;    // strata assignment for historical data
+  vector<lower=0, upper=1>[K]     a0s;           // power prior parameter for each stratum
+  vector[p]                       beta_mean;     // mean for normal initial prior on coefficients
+  vector<lower=0>[p]              beta_sd;       // sd for normal initial prior on coefficients
+  vector[J]                       hazard_mean;   // location parameter for half-normal prior on baseline hazards
+  vector<lower=0>[J]              hazard_sd;     // scale parameter for half-normal prior on baseline hazards
+  real                            logit_p_cured_mean; // mean for normal prior on logit_p_cured
+  real<lower=0>                   logit_p_cured_sd;   // sd for normal prior on logit_p_cured
+  int<lower=0,upper=1>            get_loglik;    // whether to generate log-likelihood matrix
+}
+transformed data{
+  vector[n1] logcens;
+  vector[n0] logcens0;
+  real lognc_hazard = normal_lccdf(0 | hazard_mean, hazard_sd);
+
+  // compute censoring indicators
+  for ( i in 1:n1 )
+    logcens[i] = log(1 - death_ind[i]);
+
+  for ( i in 1:n0 )
+    logcens0[i] = log(1 - death_ind0[i]);
+}
+parameters {
+  vector[K]             logit_p_curedVec; // Kx1 vector of logit of proportions of cured individuals
+  matrix<lower=0>[J,K]  lambdaMat;        // JxK matrix of hazards for each interval
+  matrix[p,K]           betaMat;          // pxK matrix of regression coefficients; p = number of covars, K = number of strata
+}
+transformed parameters{
+  vector<lower=0,upper=1>[K] p_curedVec = inv_logit(logit_p_curedVec); // Kx1 vector of proportions of cured individuals
+}
+model {
+  matrix[n1,K]  etaMat        = X1 * betaMat;
+  matrix[n0,K]  eta0Mat       = X0 * betaMat;
+  matrix[J,K]   log_lambdaMat = log(lambdaMat);
+  matrix[J-1,K] cumblhazMat;
+
+  // noninformative initial prior
+  for ( k in 1:K ) {
+    target += normal_lpdf(betaMat[, k] | beta_mean, beta_sd);
+    // half-normal prior on baseline hazards
+    target += normal_lpdf(lambdaMat[, k] | hazard_mean, hazard_sd) - lognc_hazard;
+
+    // compute lambda[j] * (s[j] * s[j-1])
+    cumblhazMat[, k] = cumulative_sum( lambdaMat[1:(J-1), k] .* ( breaks[2:J] - breaks[1:(J-1)] ) );
+  }
+
+  // prior on logit_p_curedVec
+  target += normal_lpdf(logit_p_curedVec | logit_p_cured_mean, logit_p_cured_sd);
+
+  // power prior
+  for ( i in 1:n0 ){
+    target += a0s[ stratumID0[i] ] * log_mix(
+      p_curedVec[ stratumID0[i] ],
+      logcens0[i],
+      pwe_lpdf(y0[i] | eta0Mat[i, stratumID0[i]], lambdaMat[, stratumID0[i]], log_lambdaMat[, stratumID0[i]], breaks, intindx0[i], death_ind0[i], cumblhazMat[, stratumID0[i]])
+    );
+  }
+
+  // current data likelihood
+  for ( i in 1:n1 ){
+    target += log_mix(
+      p_curedVec[ stratumID[i] ],
+      logcens[i],
+      pwe_lpdf(y1[i] | etaMat[i, stratumID[i]], lambdaMat[, stratumID[i]], log_lambdaMat[, stratumID[i]], breaks, intindx[i], death_ind[i], cumblhazMat[, stratumID[i]])
+    );
+  }
+}
+generated quantities{
+  vector[(get_loglik == 1) ? n1 : 0] log_lik; //to generate log likelihood matrices
+  matrix[n1,K]  etaMat        = X1 * betaMat;
+  matrix[J,K]   log_lambdaMat = log(lambdaMat);
+  matrix[J-1,K] cumblhazMat;
+
+  for ( k in 1:K ) {
+    // compute lambda[j] * (s[j] * s[j-1])
+    cumblhazMat[, k] = cumulative_sum( lambdaMat[1:(J-1), k] .* ( breaks[2:J] - breaks[1:(J-1)] ) );
+  }
+
+  if ( get_loglik == 1 ) {
+    for( i in 1:n1 ){
+      log_lik[i] = log_mix(
+        p_curedVec[ stratumID[i] ],
+        logcens[i],
+        pwe_lpdf(y1[i] | etaMat[i, stratumID[i]], lambdaMat[, stratumID[i]], log_lambdaMat[, stratumID[i]], breaks, intindx[i], death_ind[i], cumblhazMat[, stratumID[i]])
+      );
+    }
+  }
+}
+
